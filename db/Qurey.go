1b6364580cf488c6410151406c4ddc2f8365d853
tymon42 <tymon42@outlook.com>
2021-08-15 13:44:31 +0800

    Reform

--- db/Qurey.go
+++ db/Qurey.go
@@ -7,49 +7,49 @@ import (
 )
 
 func Query_uuid_by_phone(phone string) string {
-    db := Open_db()
-    if err:= db.Ping(); err != nil {
-        log.Panicln(err)
-    }
-    defer db.Close()
+	db := Open_db()
+	if err := db.Ping(); err != nil {
+		log.Panicln(err)
+	}
+	defer db.Close()
 
 	rows, err := db.Query("SELECT phone, uuid FROM users")
-    checkErr(err)
-	
-	for rows.Next() {
-        var phone_number string
-        var uuid string
-        err = rows.Scan(&phone_number, &uuid)
-        checkErr(err)
-        if phone_number == phone{
-            return uuid
-        }
-    }
-    defer rows.Close()
+	checkErr(err)
 
-    return "The phone is not exist."
+	for rows.Next() {
+		var phone_number string
+		var uuid string
+		err = rows.Scan(&phone_number, &uuid)
+		checkErr(err)
+		if phone_number == phone {
+			return uuid
+		}
+	}
+	defer rows.Close()
+
+	return "The phone is not exist."
 }
 
 func If_old_user(uuid, phone string) bool {
-    db := Open_db()
-    if err := db.Ping(); err != nil {
-        log.Panicln(err)
-    }
-    defer db.Close()
+	db := Open_db()
+	if err := db.Ping(); err != nil {
+		log.Panicln(err)
+	}
+	defer db.Close()
 
-    rows, err := db.Query("SELECT phone, uuid FROM users")
-    checkErr(err)
-	
+	rows, err := db.Query("SELECT phone, uuid FROM users")
+	checkErr(err)
+
 	for rows.Next() {
-        var phone_number string
-        var id string
-        err = rows.Scan(&phone_number, &uuid)
-        checkErr(err)
-        if phone_number == phone || id == uuid{
-            return true
-        }
-    }
-    defer rows.Close()
+		var phone_number string
+		var id string
+		err = rows.Scan(&phone_number, &uuid)
+		checkErr(err)
+		if phone_number == phone || id == uuid {
+			return true
+		}
+	}
+	defer rows.Close()
 
-    return false
+	return false
 }
