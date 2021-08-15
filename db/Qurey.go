3a61591fdba86c346165d9efa13f97e2fa00aa5a
tymon42 <tymon42@outlook.com>
2021-08-15 13:44:03 +0800

    判断是否为老用户函数

--- db/Qurey.go
+++ db/Qurey.go
@@ -29,3 +29,27 @@ func Query_uuid_by_phone(phone string) string {
 
     return "The phone is not exist."
 }
+
+func If_old_user(uuid, phone string) bool {
+    db := Open_db()
+    if err := db.Ping(); err != nil {
+        log.Panicln(err)
+    }
+    defer db.Close()
+
+    rows, err := db.Query("SELECT phone, uuid FROM users")
+    checkErr(err)
+	
+	for rows.Next() {
+        var phone_number string
+        var id string
+        err = rows.Scan(&phone_number, &uuid)
+        checkErr(err)
+        if phone_number == phone || id == uuid{
+            return true
+        }
+    }
+    defer rows.Close()
+
+    return false
+}
