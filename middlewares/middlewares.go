4464ba440f1611a883bf07a16d0b783b4a368451
tymon42 <tymon42@outlook.com>
2021-08-16 19:22:46 +0800

    新增中间件跨域规则

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -7,8 +7,10 @@ import (
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Access-Control-Allow-Origin")
+		// c.Writer.Header().Set("Access-Control-Expose-Headers", "Access-Control-Allow-Origin")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
